99acb9dfaef6dcc968cf6174422174c6627d45ff
Bruno Ienne <brunoienne98@gmail.com>
2023-09-18 21:50:39 -0300

    api_filmes

--- busca_filmes/webapp/controller/Main.controller.js
+++ busca_filmes/webapp/controller/Main.controller.js
@@ -1,15 +1,80 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, JSONModel) {
         "use strict";
 
         return Controller.extend("buscafilmes.buscafilmes.controller.Main", {
             onInit: function () {
+                var oPopulares = new JSONModel();
+                var oAvaliados = new JSONModel();
+                var oBreve = new JSONModel();
+                this.getView().setModel(oPopulares, "Populares");
+                this.getView().setModel(oAvaliados, "Avaliados");
+                this.getView().setModel(oBreve, "Breve");
+                this.buscarPopulares();
+                this.buscarAvaliados();
+                this.buscarBreve();
+            },
 
-            }
+            buscarPopulares: function () {
+                var dadosModel = this.getView().getModel("Populares");
+
+                $.ajax({
+                    method: "GET",
+                    url: "https://api.themoviedb.org/3/tv/popular?language=pt-BR",
+                    headers: {
+                        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNGZlNzU1OWM2ZDVhZDU2MDk3MjFkNGVlMzBhZTRhMyIsInN1YiI6IjY0YmZkYTA4NmQ0Yzk3MDBlMjc2M2QyZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.R55_Ur73YO3RKtAA3oMTzFDNiWele1_gNayczv5Oqio'
+                    },
+                    success: function (dados) {
+                        dadosModel.setData(dados);
+                    },
+                    error: function (erro) {
+                        console.log(erro);
+                    }
+
+                });
+            },
+
+            buscarAvaliados: function () {
+                var dadosModel = this.getView().getModel("Avaliados");
+
+                $.ajax({
+                    method: "GET",
+                    url: "https://api.themoviedb.org/3/movie/top_rated?language=pt-BR",
+                    headers: {
+                        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNGZlNzU1OWM2ZDVhZDU2MDk3MjFkNGVlMzBhZTRhMyIsInN1YiI6IjY0YmZkYTA4NmQ0Yzk3MDBlMjc2M2QyZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.R55_Ur73YO3RKtAA3oMTzFDNiWele1_gNayczv5Oqio'
+                    },
+                    success: function (dados) {
+                        dadosModel.setData(dados);
+                    },
+                    error: function (erro) {
+                        console.log(erro);
+                    }
+
+                });
+            },
+
+            buscarBreve: function () {
+                var dadosModel = this.getView().getModel("Breve");
+                $.ajax({
+                    method: "GET",
+                    url: "https://api.themoviedb.org/3/movie/upcoming?language=pt-BR",
+                    headers: {
+                        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNGZlNzU1OWM2ZDVhZDU2MDk3MjFkNGVlMzBhZTRhMyIsInN1YiI6IjY0YmZkYTA4NmQ0Yzk3MDBlMjc2M2QyZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.R55_Ur73YO3RKtAA3oMTzFDNiWele1_gNayczv5Oqio'
+                    },
+                    success: function (dados) {
+                        dadosModel.setData(dados);
+                    },
+                    error: function (erro) {
+                        console.log(erro);
+                    }
+
+                });
+            },
         });
     });
